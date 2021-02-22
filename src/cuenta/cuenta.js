a846da7c339001cefcffa8ac80a52630cb5e5693
joseluisgs <joseluisgonsan@gmail.com>
2021-02-22 11:05:58 +0100

    Codigo Crear Cuenta

---
+++ src/cuenta/cuenta.js
@@ -0,0 +1,11 @@
+class Cuenta {
+  constructor() {
+    this.saldo = 0;
+  }
+
+  getSaldo() {
+    return this.saldo;
+  }
+}
+
+export default Cuenta;
--- src/cuenta/tests/cuenta.spec.js
+++ src/cuenta/tests/cuenta.spec.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-undef */
 
 // Suite de test de cuenta
+import Cuenta from '../cuenta';
+
 describe('TDD Cuenta', () => {
   test('Cuenta: Al crear una cuenta el saldo debería ser cero', () => {
     const cuenta = new Cuenta();
-    expect(cuenta.saldo()).toBe(0);
+    expect(cuenta.getSaldo()).toBe(0);
   });
 });
