ab38056032d18f122444f2e4011705a72afb43c6
joseluisgs <joseluisgonsan@gmail.com>
2021-02-22 11:10:05 +0100

    Comentarios

--- src/cuenta/cuenta.js
+++ src/cuenta/cuenta.js
@@ -1,12 +1,25 @@
+/**
+ * Clase para el maneo de una cuenta
+ */
 class Cuenta {
+  /**
+   * Costructor
+   */
   constructor() {
     this.saldo = 0;
   }
 
+  /**
+   * Devuelve el saldo
+   */
   getSaldo() {
     return this.saldo;
   }
 
+  /**
+   * Ingresa un saldo
+   * @param {number} cantidad
+   */
   ingresar(cantidad) {
     this.saldo = cantidad;
   }
